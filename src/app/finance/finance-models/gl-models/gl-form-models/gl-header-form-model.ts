d59c2b6624f8fb19dc8452def30f45a4eced6498
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-03-28 17:10:05 +0300

    GL Form class

--- src/app/finance/finance-models/gl-models/gl-form-models/gl-header-form-model.ts
+++ src/app/finance/finance-models/gl-models/gl-form-models/gl-header-form-model.ts
@@ -46,81 +46,146 @@ export class GLHeaderFormControl extends FormControl {
 export class GLHeaderFormGroup extends FormGroup {
 
     constructor() {
-        super({
+      super({
 
-
-            first_name: new GLHeaderFormControl("First Name", "first_name", "", Validators.required),
-            last_name: new GLHeaderFormControl("Last Name", "last_name", "", Validators.required),
-            date_of_birth: new GLHeaderFormControl("Birthday", "date_of_birth", "", Validators.required),
-            phone_number: new GLHeaderFormControl("Phone Number", "phone_number", "", Validators.required),
-            username: new GLHeaderFormControl("Username", "username", "", Validators.required),
-            email: new GLHeaderFormControl("Email", "email", "", Validators.compose([
-                Validators.required,
-                Validators.email
-            ])),
-            gender: new GLHeaderFormControl("Gender", "gender", "", Validators.required),
-            city: new GLHeaderFormControl("City", "city", "", Validators.required),
-            password: new GLHeaderFormControl("Password", "Password", "",
-                Validators.compose([
-                    Validators.required,
-                    // Validators.pattern("/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/"),
-                    Validators.minLength(8),
-                    Validators.maxLength(20)
-                ])),
-            confirmPassword: new GLHeaderFormControl("Confirm Password", "confirmPassword", "",
-                Validators.compose([
-                    Validators.required,
-                    // Validators.pattern("/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/"),
-                    Validators.minLength(8),
-                    Validators.maxLength(20)
-                ]))
+        chartOfAccounts: new GLHeaderFormControl("Chart of Accounts", "chartOfAccounts", "", Validators.required),
+        companyCode: new GLHeaderFormControl("Company Code", "companyCode", "", Validators.required),
+        accountNumber: new GLHeaderFormControl("Account Number", "accountNumber", "", Validators.required),
+        accountType: new GLHeaderFormControl("Account Type", "accountType", "", Validators.required),
+        accountGroup: new GLHeaderFormControl("Account Group", "accountGroup", "", Validators.required),
+        reconciliationAccountType: new GLHeaderFormControl("Reconciliation Account Type", "reconciliationAccountType", "", Validators.required),
+        alternativeGLAccount: new GLHeaderFormControl("Alternative GL Account", "alternativeGLAccount", "", Validators.required),
+        shortDescription: new GLHeaderFormControl("Short Description", "shortDescription", "", Validators.required),
+        longDescription: new GLHeaderFormControl("Long Description", "longDescription", "", Validators.required),
+        tradingPartner: new GLHeaderFormControl("Trading partner", "tradingPartner", "", Validators.required),
+        groupAccountNumber: new GLHeaderFormControl("Group Account Number", "groupAccountNumber", "", Validators.required),
+        blockedForPosting: new GLHeaderFormControl("Blocked for posting", "blockedForPosting", "", Validators.required),
+        markedForDeletion: new GLHeaderFormControl("Marked for deletion", "markedForDeletion", "", Validators.required),
+        accountCurrency: new GLHeaderFormControl("Account Currency", "accountCurrency", "", Validators.required),
+        exchangeRateKey: new GLHeaderFormControl("Exchange Rate Key", "exchangeRateKey", "", Validators.required),
+        taxCategory: new GLHeaderFormControl("Tax Category", "taxCategory", "", Validators.required),
+        postingWithoutTaxAllowed: new GLHeaderFormControl("Posting without tax allowed", "postingWithoutTaxAllowed", "", Validators.required),
+        openItemManagement: new GLHeaderFormControl("Open Item Management", "openItemManagement", "", Validators.required),
+        sortKey: new GLHeaderFormControl("Sort Key", "sortKey", "", Validators.required),
+        authorizationGroup: new GLHeaderFormControl("Authorization Group", "authorizationGroup", "", Validators.required),
+        fieldStatusGroup: new GLHeaderFormControl("Field Status Group", "fieldStatusGroup", "", Validators.required),
+        postAutomaticallyOnly: new GLHeaderFormControl("Post Automatically only", "postAutomaticallyOnly", "", Validators.required),
+        relevantToCashFlow: new GLHeaderFormControl("Relevant to Cashflow", "relevantToCashFlow", "", Validators.required),
+        houseBank: new GLHeaderFormControl("House Bank", "houseBank", "", Validators.required),
+        houseBankAccountID: new GLHeaderFormControl("House Bank Account ID", "houseBankAccountID", "", Validators.required),
+        interestIndicator: new GLHeaderFormControl("InterestIndicator", "interestIndicator", "", Validators.required),
+        interestCalculationFrequency: new GLHeaderFormControl("Interest Calculation Frequency", "interestCalculationFrequency", "", Validators.required),
+        lastDateOfInterestCalculation: new GLHeaderFormControl("Last day of interest calculation", "lastDateOfInterestCalculation", "", Validators.required),
+        controllingArea: new GLHeaderFormControl("Controlling Area", "controllingArea", "", Validators.required),
+        costElement: new GLHeaderFormControl("Cost Element", "costElement", "", Validators.required),
+        unitOfMeasure: new GLHeaderFormControl("Unit of Measure", "unitOfMeasure", "", Validators.required),
 
         });
     }
 
-    get signUpFormControls(): GLHeaderFormControl[] {
+    get glHeaderFormControls(): GLHeaderFormControl[] {
         return Object.keys(this.controls).map(k => this.controls[k] as GLHeaderFormControl);
     }
 
 
-    getfirstNameValidationMessages(first_name: string): string[] {
-        return (this.controls['first_name'] as GLHeaderFormControl).getValidationMessages();
+    getChartOfAccountsValidationMessages(chartOfAccounts: string): string[] {
+        return (this.controls['chartOfAccounts'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getlastNameValidationMessages(last_name: string): string[] {
-        return (this.controls['last_name'] as GLHeaderFormControl).getValidationMessages();
+    getCompanyCodeValidationMessages(companyCode: string): string[] {
+        return (this.controls['companyCode'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getBirthDayValidationMessages(date_of_birth: string): string[] {
-        return (this.controls['date_of_birth'] as GLHeaderFormControl).getValidationMessages();
+    getAccountNumberValidationMessages(accountNumber: string): string[] {
+        return (this.controls['accountNumber'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getPhoneNumberValidationMessages(phone_number: string): string[] {
-        return (this.controls['phone_number'] as GLHeaderFormControl).getValidationMessages();
+    getAccountTypeValidationMessages(accountType: string): string[] {
+        return (this.controls['accountType'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getUserNameValidationMessages(username: string): string[] {
-        return (this.controls['username'] as GLHeaderFormControl).getValidationMessages();
+    getAccountGroupValidationMessages(accountGroup: string): string[] {
+        return (this.controls['accountGroup'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getEmailValidationMessages(email: string): string[] {
-        return (this.controls['email'] as GLHeaderFormControl).getValidationMessages();
+    getReconciliationAccountTypeValidationMessages(reconciliationAccountType: string): string[] {
+        return (this.controls['reconciliationAccountType'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getGenderValidationMessages(gender: string): string[] {
-        return (this.controls['gender'] as GLHeaderFormControl).getValidationMessages();
+    getAlternativeGLAccountValidationMessages(alternativeGLAccount: string): string[] {
+        return (this.controls['alternativeGLAccount'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getCityValidationMessages(city: string): string[] {
-        return (this.controls['city'] as GLHeaderFormControl).getValidationMessages();
+    getShortDescriptionValidationMessages(shortDescription: string): string[] {
+        return (this.controls['shortDescription'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getPasswordValidationMessages(password: string): string[] {
-        return (this.controls['password'] as GLHeaderFormControl).getValidationMessages();
+    getLongDescriptionValidationMessages(longDescription: string): string[] {
+        return (this.controls['longDescription'] as GLHeaderFormControl).getValidationMessages();
     }
 
-    getConfirmPasswordValidationMessages(confirmPassword: string): string[] {
-        return (this.controls['confirmPassword'] as GLHeaderFormControl).getValidationMessages();
+    getGroupAccountNumberValidationMessages(groupAccountNumber: string): string[] {
+        return (this.controls['groupAccountNumber'] as GLHeaderFormControl).getValidationMessages();
+    }
+     getBlockedForPostingValidationMessages(blockedForPosting: string): string[] {
+        return (this.controls['blockedForPosting'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getMarkedForDeletionValidationMessages(markedForDeletion: string): string[] {
+        return (this.controls['markedForDeletion'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getAccountCurrencyValidationMessages(accountCurrency: string): string[] {
+        return (this.controls['accountCurrency'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getExchangeRateKeyValidationMessages(exchangeRateKey: string): string[] {
+        return (this.controls['exchangeRateKey'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getTaxCategoryValidationMessages(taxCategory: string): string[] {
+        return (this.controls['taxCategory'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getPostingWithoutTaxAllowedValidationMessages(postingWithoutTaxAllowed: string): string[] {
+        return (this.controls['postingWithoutTaxAllowed'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getOpenItemManagementValidationMessages(openItemManagement: string): string[] {
+        return (this.controls['openItemManagement'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getSortKeyValidationMessages(sortKey: string): string[] {
+        return (this.controls['sortKey'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getAuthorizationGroupValidationMessages(authorizationGroup: string): string[] {
+        return (this.controls['authorizationGroup'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getfieldStatusGroupValidationMessages(fieldStatusGroup: string): string[] {
+        return (this.controls['fieldStatusGroup'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getPostAutomaticallyOnlyValidationMessages(postAutomaticallyOnly: string): string[] {
+        return (this.controls['postAutomaticallyOnly'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getRelevantToCashFlowValidationMessages(relevantToCashFlow: string): string[] {
+        return (this.controls['relevantToCashFlow'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getHouseBankValidationMessages(houseBank: string): string[] {
+        return (this.controls['houseBank'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getHouseBankAccountIDValidationMessages(houseBankAccountID: string): string[] {
+        return (this.controls['houseBankAccountID'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getInterestCalculationFrequencyValidationMessages(interestCalculationFrequency: string): string[] {
+        return (this.controls['interestCalculationFrequency'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getLastDateOfInterestCalculationValidationMessages(lastDateOfInterestCalculation: string): string[] {
+        return (this.controls['lastDateOfInterestCalculation'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getControllingAreaValidationMessages(controllingArea: string): string[] {
+        return (this.controls['controllingArea'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getCostElementValidationMessages(costElement: string): string[] {
+        return (this.controls['costElement'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getUnitOfMeasureValidationMessages(unitOfMeasure: string): string[] {
+        return (this.controls['unitOfMeasure'] as GLHeaderFormControl).getValidationMessages();
+     }
+     getInterestIndicatorValidationMessages(interestIndicator: string): string[] {
+        return (this.controls['interestIndicator'] as GLHeaderFormControl).getValidationMessages();
     }
 
     getFormValidationMessages(): string[] {
