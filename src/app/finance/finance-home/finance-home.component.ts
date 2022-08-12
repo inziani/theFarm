92abae92b82a2e529793867c3596ed67fc5cb544
“inziani” <ValentineRobai.Inziani@gmail.com>
2022-08-12 16:56:42 +0300

    Finance homepage

--- src/app/finance/finance-home/finance-home.component.ts
+++ src/app/finance/finance-home/finance-home.component.ts
@@ -12,4 +12,40 @@ export class FinanceHomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelectCompany() {
+
+    alert('The link works!')
+
+  }
+
+  onSelectCompanyCode() {
+
+    alert('The link works!')
+
+  }
+
+  onSelectChartofAccounts() {
+
+    alert('The link works!')
+
+  }
+
+  onSelectReportingArea() {
+
+    alert('The link works!')
+
+  }
+
+  onSelectControllingArea() {
+
+    alert('The link works!')
+
+  }
+
+  onSelectBusinessArea() {
+
+    alert('The link works!')
+
+  }
+
 }
