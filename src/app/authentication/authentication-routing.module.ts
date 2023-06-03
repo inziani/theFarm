f17a007e00650a06e343e8ec61e880e9b5677ed2
“inziani” <ValentineRobai.Inziani@gmail.com>
2023-06-03 15:08:00 +0300

    Homepage changes

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -15,9 +15,8 @@ const routes: Routes = [
   },
 ];
 
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthenticationRoutingModule { }
+export class AuthenticationRoutingModule {}
