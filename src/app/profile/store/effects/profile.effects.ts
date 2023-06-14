2fdee472a9b077f56fced480e04e59777905b48e
“inziani” <ValentineRobai.Inziani@gmail.com>
2023-06-14 16:51:57 +0300

    Activity Effects Creation

---
+++ src/app/profile/store/effects/profile.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { ActivitysService } from '@app/_helpers/services/activitys.service';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+
+import * as ActivityActions from '../actions/profile.actions';
+import { map, mergeMap } from 'rxjs';
+import { Activity } from '@app/profile/todo/models/activity.model';
+
+@Injectable()
+export class ProfileEffects {
+  constructor(
+    private _actions$: Actions,
+    private _activityService: ActivitysService
+  ) {}
+
+  public loadActivities$ = createEffect(() => {
+    return this._actions$.pipe(
+      ofType(ActivityActions.fetchActivityData),
+      mergeMap(() =>
+        this._activityService
+          .fetchActivityData()
+          .pipe(
+            map((activity) =>
+              ActivityActions.fetchActivityDataSuccess({ activity })
+            )
+          )
+      )
+    );
+  });
+}
