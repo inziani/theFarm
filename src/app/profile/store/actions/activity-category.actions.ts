196afd021dccf5cdd5f09f039ed12d89862cf351
Valentine Robai Inziani <67521369+inziani@users.noreply.github.com>
2023-09-01 10:06:19 +0300

    ActivityCategories refactor with enties

--- src/app/profile/store/actions/activity-category.actions.ts
+++ src/app/profile/store/actions/activity-category.actions.ts
@@ -1,10 +1,23 @@
 import { ActivityCategory } from '@app/profile/todo/models/activity-category.models';
+import { Update } from '@ngrx/entity';
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
 export const ActivityCategoryPageActions = createActionGroup({
   source: 'Activity Category Page',
   events: {
-    '[ActivityCategory] Load Activity Categories': emptyProps(),
+    '[ActivityCategoryPage] Load Activity Categories': emptyProps(),
+    '[ActivityCategoryPage] Select Single Activity Category': props<{
+      activityCategoryId: number;
+    }>(),
+    '[ActivityCategoryPage] Create Activity Category': props<{
+      activityCategory: ActivityCategory;
+    }>(),
+    '[ActivityCategoryPage] Edit Activity Category': props<{
+      activityCategory: ActivityCategory;
+    }>(),
+    '[ActivityCategoryPage] Delete Activity Category': props<{
+      ActivityCategoryId: number;
+    }>(),
   },
 });
 
@@ -12,9 +25,27 @@ export const ActivityCategoryAPIActions = createActionGroup({
   source: 'Activity Category API',
   events: {
     '[ActivityCategoryAPI] Load Activity Categories Success': props<{
-      activityCategoryList: ActivityCategory[];
+      activityCategories: ActivityCategory[];
     }>(),
     '[ActivityCategoryAPI] Load Activity Categories Failure': props<{
+      errorMessage: string;
+    }>(),
+    '[ActivityCategoryAPI] Create Activity Category Success': props<{
+      activityCategory: ActivityCategory;
+    }>(),
+    '[ActivityCategoryAPI] Create Activity Category Fail': props<{
+      errorMessage: string;
+    }>(),
+    '[ActivityCategoryAPI] Edit Activity Category Success': props<{
+      activityCategory: Update<ActivityCategory>;
+    }>(),
+    '[ActivityCategoryAPI] Edit Activity Category Fail': props<{
+      errorMessage: string;
+    }>(),
+    '[ActivityCategoryAPI] Delete Activity Category Success': props<{
+      activityCategoryId: number;
+    }>(),
+    '[ActivityCategoryAPI] Delete Activity Category Fail': props<{
       errorMessage: string;
     }>(),
   },
---
+++ src/app/profile/store/reducers/activity-category.reducer.ts
@@ -0,0 +1,130 @@
+import { createReducer, on } from '@ngrx/store';
+import { EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+
+import { ActivityCategoryState } from '../state/activity-category.state';
+import {
+  ActivityPageActions,
+  ActivityAPIActions,
+} from '../actions/activity.actions';
+import { Activity } from '@app/profile/todo/models/activity.model';
+
+export const activityAdapter: EntityAdapter<Activity> =
+  createEntityAdapter<Activity>({
+    selectId,
+    // sortByTitle
+  });
+
+export function selectId(activity: Activity): number {
+  return activity.id;
+}
+
+export function sortByTitle(a: Activity, b: Activity): string {
+  return a.title.localeCompare(b.title).toString();
+}
+
+const { selectIds, selectAll, selectEntities, selectTotal } =
+  activityAdapter.getSelectors();
+
+export const selectCurrentActivityId = selectIds;
+export const selectActivityEntities = selectEntities;
+export const selectActivities = selectAll;
+
+export const initialState: ActivityState = activityAdapter.getInitialState({
+  loading: false,
+  errorMessage: '',
+});
+
+export const activityReducer = createReducer<ActivityState>(
+  initialState,
+
+  on(
+    ActivityPageActions['[ActivityPage]LoadActivities'],
+
+    (state) =>
+      activityAdapter.setAll([], { ...state, loading: true, errorMessage: '' })
+  ),
+  on(
+    ActivityAPIActions['[ActivityAPI]LoadActivitesSuccess'],
+    (state, { activities }) =>
+      activityAdapter.setAll(activities, {
+        ...state,
+        loading: false,
+        error: '',
+      })
+  ),
+  on(
+    ActivityAPIActions['[ActivityAPI]LoadActivitiesFailure'],
+    (state, { errorMessage }) =>
+      activityAdapter.setAll([], {
+        ...state,
+        loading: false,
+        error: errorMessage,
+      })
+  ),
+
+  on(ActivityPageActions['[ActivityPage]CreateActivity'], (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: '',
+  })),
+  on(
+    ActivityAPIActions['[ActivityAPI]CreateActivitySuccess'],
+    (state, { activity }) =>
+      activityAdapter.addOne(activity, {
+        ...state,
+        loading: false,
+        errorMessage: '',
+      })
+  ),
+  on(
+    ActivityAPIActions['[ActivityAPI]CreateActivityFail'],
+    (state, { errorMessage }) => ({
+      ...state,
+      loading: false,
+      errorMessage: errorMessage,
+    })
+  ),
+  on(ActivityPageActions['[ActivityPage]EditActivity'], (state) => ({
+    ...state,
+    loading: true,
+    errorMessage: '',
+  })),
+  on(
+    ActivityAPIActions['[ActivityAPI]EditActivitySuccess'],
+    (state, { activity }) =>
+      activityAdapter.updateOne(activity, {
+        ...state,
+        loading: false,
+        error: '',
+      })
+  ),
+  on(
+    ActivityAPIActions['[ActivityAPI]EditActivityFail'],
+    (state, { errorMessage }) => ({
+      ...state,
+      loading: false,
+      errorMessage: errorMessage,
+    })
+  ),
+  on(ActivityPageActions['[ActivityPage]DeleteActivity'], (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(
+    ActivityAPIActions['[ActivityAPI]DeleteActivitySuccess'],
+    (state, { activityId }) =>
+      activityAdapter.removeOne(activityId, {
+        ...state,
+        loading: false,
+        errorMessage: '',
+      })
+  ),
+  on(
+    ActivityAPIActions['[ActivityAPI]DeleteActivityFail'],
+    (state, { errorMessage }) => ({
+      ...state,
+      loading: false,
+      errorMessage: errorMessage,
+    })
+  )
+);
